3ca99a7923becd9970a572321ce492a536f8a9bc
Ted L <dstrike@gmail.com>
2021-09-29 12:07:19 -0400

    Update Remora

--- zBot/Commands/StatusCommands.cs
+++ zBot/Commands/StatusCommands.cs
@@ -3,22 +3,19 @@ using System.Drawing;
 using System.Threading.Tasks;
 using Remora.Commands.Attributes;
 using Remora.Commands.Groups;
-using Remora.Discord.API.Abstractions.Rest;
 using Remora.Discord.API.Objects;
-using Remora.Discord.Commands.Contexts;
+using Remora.Discord.Commands.Feedback.Services;
 using Remora.Results;
 
 namespace zBot.Commands
 {
     public class StatusCommands : CommandGroup
     {
-        private readonly IDiscordRestWebhookAPI _webhookApi;
-        private readonly InteractionContext _interactionContext;
+        private readonly FeedbackService _feedbackService;
 
-        public StatusCommands(IDiscordRestWebhookAPI webhookApi, InteractionContext interactionContext)
+        public StatusCommands(FeedbackService feedbackService)
         {
-            _webhookApi = webhookApi;
-            _interactionContext = interactionContext;
+            _feedbackService = feedbackService;
         }
 
         [Command("ping")]
@@ -26,13 +23,7 @@ namespace zBot.Commands
         public async Task<IResult> PostPongStatusAsync()
         {
             var embed = new Embed(Description: "Pong!", Colour: Color.LawnGreen);
-            var reply = await _webhookApi.CreateFollowupMessageAsync
-            (
-                _interactionContext.ApplicationID,
-                _interactionContext.Token,
-                embeds: new[] {embed},
-                ct: CancellationToken
-            );
+            var reply = await _feedbackService.SendContextualEmbedAsync(embed, this.CancellationToken);
             return !reply.IsSuccess
                 ? Result.FromError(reply)
                 : Result.FromSuccess();
